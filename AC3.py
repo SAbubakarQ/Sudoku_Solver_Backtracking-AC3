ce465697524f7deca8dae215bd34df3687d428bc
Abubakar Qasim <65397022+SAbubakarQ@users.noreply.github.com>
2022-02-24 16:22:29 -0600

    Added full AC3 algorithm

--- AC3.py
+++ AC3.py
@@ -7,8 +7,19 @@ import Utility
 #===============================================================#
 
 def AC3(board):
-    print("\nWorking\n")
-    Utility.revise(1, 2)
+    
+    # board to a queue
+    queue = board[:]
 
+    # repeats until queue is empty
+    while queue:
+        # Take the first 'arc' off the queue
+        (x, y) = queue.pop(0)
 
+        # Making x arc consistent with y
+        revised = Utility.revise(board, x, y)
 
+        # If the x domain changed:
+        if revised:
+            neighbors = [neighbor for neighbor in board if neighbor[1] == x]
+            queue = queue + neighbors
